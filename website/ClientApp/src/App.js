95c47f550450e112ea70fdfe21df4303a5af99b4
Vova <romaniuk.volodymyr.clg@chnu.edu.ua>
2022-01-14 22:08:21 +0200

    header 2.0 and footer

--- website/ClientApp/src/App.js
+++ website/ClientApp/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router';
 import Header from "./components/Shared/Header";
+import Footer from "./components/Shared/Footer";
 import AdminPage from './components/AdminPage/AdminPage';
 import './custom.css'
 
@@ -11,6 +12,7 @@ export default class App extends Component {
     return (
       <div className="App"> 
        <Header/>
+       <Footer/>
         <Route path = '/admin' component={AdminPage} />
       </div>
     );
--- website/ClientApp/src/components/Shared/Header.js
+++ website/ClientApp/src/components/Shared/Header.js
@@ -4,7 +4,7 @@ function Header() {
     return (
         <header class="header" >
             <div className="body_header">
-                <div class="container_header">
+                <div class="container_all">
                      <div className="mainInfo"> 
                          <div className="About_us">
                              <div className="text">
@@ -19,14 +19,20 @@ function Header() {
                  </div>
             </div>
             <div className="menu_header">
-            <div class="container_header">
-                  <div class="menu">
-                             <p>Новини</p>
-                             <p>Кращі студенти </p>
-                             <p>Спеціальності</p>
-                             <p>Студенту</p>
-                         </div>
-                 </div>
+                <div class="container_all">
+                    <div class="menu">
+                             
+                                <p>Новини</p>
+                                <p>Кращі студенти </p>
+                                <p>Спеціальності</p>
+                                <p>Студенту</p>
+                                <p>Абітурієнту</p>
+                                <p>Історія
+
+                                </p>
+                            
+                    </div>
+                </div>
             </div>
         </header> 
     )
