e5fa6f09c72c631dc7d4195c30646d79cf7bba0a
Maks <lion20914king@gmail.com>
2022-01-18 23:54:59 +0200

    Css

--- website/ClientApp/src/components/MainPage/components/Carousel.js
+++ website/ClientApp/src/components/MainPage/components/Carousel.js
@@ -3,7 +3,7 @@ import React from 'react'
 import Slider from 'react-slick'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
-
+import '../components/Carousel.css'
 export default function Carousel() {
 
   const sliderSettings = {
@@ -15,23 +15,23 @@ export default function Carousel() {
 
   const hotelCards = [
     {
-      imageSrc:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRD2zWws5ARkXyYCsXlXcUohPw5EEr7ZNWhBQ&usqp=CAU',
+      
+       imageSrc:  'https://nevseoboi.com.ua/uploads/posts/2011-09/1316735818_2_www.nevseoboi.com.ua.jpg',
       
     },
     {
-      imageSrc:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRD2zWws5ARkXyYCsXlXcUohPw5EEr7ZNWhBQ&usqp=CAU',
+      
+       imageSrc:  'https://web.kpi.kharkov.ua/emmb/wp-content/uploads/sites/185/2018/08/ucheba-za-rubezom-1080x480.jpg',
       
     },
     {
-      imageSrc:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRD2zWws5ARkXyYCsXlXcUohPw5EEr7ZNWhBQ&usqp=CAU',
-      
+     
+      imageSrc:  'http://molbuk.ua/uploads/posts/2013-03/1363095587_tehn.jpg',
+     
     },
     {
-      imageSrc:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRD2zWws5ARkXyYCsXlXcUohPw5EEr7ZNWhBQ&usqp=CAU',
+      
+       imageSrc:  'https://www.google.com/url?sa=i&url=https%3A%2F%2Fweb.kpi.kharkov.ua%2Femmb%2F2018%2F08%2F28%2Fposvyachennya-v-studenti%2F&psig=AOvVaw2oLztEH4cLPeMa6vadQEVD&ust=1642623924127000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCLjTyo-RvPUCFQAAAAAdAAAAABAD',
       
     },
   ]
@@ -42,9 +42,7 @@ export default function Carousel() {
           
         {hotelCards.map((card, index) => (
           <div className="image_info" key={index}>
-           
             <img src={card.imageSrc}  />
-            <button className='btn'>Buy Now</button>
           </div>
         ))}
       </Slider>
