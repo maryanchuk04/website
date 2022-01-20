f66c1c41711ebc8376a7121de2247f3116cd174d
Maks <lion20914king@gmail.com>
2022-01-20 12:05:28 +0200

    add

--- website/ClientApp/src/App.js
+++ website/ClientApp/src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Shared/Header";
 import Footer from "./components/Shared/Footer";
 import Mainpage from "./components/MainPage/Mainpage";
 import AdminPage from './components/AdminPage/AdminPage';
+import Best from './components/AdminPage/components/Best';
 import './custom.css'
 
 export default class App extends Component {
@@ -16,6 +17,7 @@ export default class App extends Component {
         <Routes>
           <Route path = '/' exact element = {<React.Fragment><Header/><Mainpage/><Footer/></React.Fragment>} />          
           <Route path = '/admin' exact element={<AdminPage/>} />
+          <Route path = '/admin/best' exact element={<Best/>} />
         </Routes>
       </BrowserRouter>
        
