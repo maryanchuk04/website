a9432d3ac573f7a9cdb4c066e26a7117898ea974
MaksMaryanchuk <lion20914king@gmail.com>
2022-02-01 22:10:06 +0200

    Slider, CollegeActivity, Student, History BACK DONE

--- website/Controllers/HistoryController.cs
+++ website/Controllers/HistoryController.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using website.Models;
+using website.Services;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace website.Controllers
 {
@@ -12,11 +13,46 @@ namespace website.Controllers
     [Route("/history")]
     public class HistoryController : Controller
     {
+        private readonly HistoryServices _history;
 
-
-        public HistoryController()
+        public HistoryController(HistoryServices history)
         {
-
+            _history = history;
         }
+        
+        [HttpGet("/history")]
+        public ActionResult GetAll()
+        {
+            return Ok(_history.GetAll());
+        }
+
+        [HttpGet("/history/{id}")]
+        public ActionResult GetOne(string Id)
+        {
+            return Ok(_history.GetByID(Id));
+        }
+
+        [HttpPost("/history/add")]
+        public ActionResult Add([FromBody] History history)
+        {
+            try
+            {
+                _history.Insert(history);
+                return Ok(history);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpDelete("/history/delete/{id}")]
+        public ActionResult Delete(string id)
+        {
+            _history.Delete(id);
+            return Ok();
+        }
+
+
     }
 }
---
+++ website/Services/HistoryServices.cs
@@ -0,0 +1,55 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using website.Models;
+
+namespace website.Services
+{
+    public class HistoryServices : IDataServices<History>
+    {
+        private readonly IMongoCollection<History> _history;
+
+        public HistoryServices(IMongoClient client)
+        {
+            var db = client.GetDatabase("College");
+            _history = db.GetCollection<History>("History");
+        }
+        public void Delete(string ID)
+        {
+            _history.FindOneAndDelete(x => x.id == ID);
+        }
+
+        public IEnumerable<History> GetAll()
+        {
+            return _history.Find(x => true).ToEnumerable();
+        }
+
+        public History GetByID(string Id)
+        {
+            return _history.Find(x => x.id == Id).FirstOrDefault();
+
+        }
+
+        public void Insert(History obj)
+        {
+            _history.InsertOne(obj);
+        }
+
+        public History Save(History obj)
+        {
+            if (obj != null)
+            {
+                _history.ReplaceOne(x => x.id == obj.id, obj);
+                return obj;
+            }
+            else return null;
+        }
+
+        public void Update(History obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
