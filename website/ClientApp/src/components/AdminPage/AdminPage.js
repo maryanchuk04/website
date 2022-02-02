cb30f112b944e3ad6392b919348ff8a4429ee7c7
Vova <romaniuk.volodymyr.clg@chnu.edu.ua>
2022-02-02 21:34:58 +0200

    adminka+++-

--- website/ClientApp/src/components/AdminPage/AdminPage.js
+++ website/ClientApp/src/components/AdminPage/AdminPage.js
@@ -11,13 +11,20 @@ function AdminPage() {
     const [warning,setWarnings] = useState([])
     const [state,setState] = useState()
 
+    const [showForm, setShowForm] = useState(false);
+
+    const [showPage1, setShowPage1] = useState(false);
+
+
     useEffect(async ()=>{
         const res1 = await axios.get("http://localhost:5000/student")
         //barabukli
         console.log(res1.data)
         setStudent(res1.data);
     },[])
-
+    const firstclick=((i)=>{
+       alert("HELLO")
+    })
     const StudentClick=((i)=>{
         setState(1);
     })
@@ -59,7 +66,7 @@ function AdminPage() {
 
                     <div className="main_menu">
                         <div className="admin_menu_title">
-                            Меню
+                            <p>   Меню</p> 
                         </div>
                       <ul>
                           <li>Головна</li>
@@ -86,22 +93,59 @@ function AdminPage() {
                   
                 </div>
                 <div className="other_admin">
-
+                  <div className="admin_page1">
+                        <div
+					          class="background_page1"
+					           style={{
+					        	transform: `scale(${
+					        		showPage1 ? 1 : 0
+					        	})`,
+					        }}>
+                                <div id="cancel-btn" ><i className="fas fa-times" onClick={() => setShowPage1(false)}></i></div>
+                            <div className="admin_page1_info">
+                                <input type="text"  className="elemen_admin  elemen_admin_input" placeholder="Заголовок"/>
+                                <textarea name="text" className="elemen_admin elemen_admin_textarea" id=""  placeholder="Текст"></textarea>
+                             </div>
+                             <div className="admin_page1_buttons">
+                                <input type="file" />
+                                <button>Зберегти</button>
+                            </div>
+                        </div>
+                  </div>
                 </div>
                 <div className="button_admin">
-                      <div className="loc_btn">
-                          <div className="select_img">
-                             <button>1</button>
-                             <button>2</button>
-                             <button>3</button>
-                          </div>
-                          <button className="butn">Додати</button>
+                      
+                                    <div
+                                    class="background"
+                                    style={{
+                                        transform: `scale(${
+                                            showForm ? 1 : 0
+                                        })`,
+                                    }}>    
+                                  
+                                  <div id="cancel-btn" ><i className="fas fa-times" onClick={() => setShowForm(false)}></i></div>
+                                  
+                                  <div className="popup">
+                                   <div className="sub_buttons">
+                                        <button onClick={() => setShowPage1(true)} >1</button>
+                                        <button>2</button>
+                                        <button>3</button>
+                                    </div>
+                                </div>       
+                            </div>
+                        
+                          
+                           <div className="buttons">  
+                          <button className="butn " onClick={() => setShowForm(true)}>Додати</button>
                           <button className="butn">Видалити</button>
                           <button className="butn">Редагувати</button>
-                      </div>
+                          </div> 
                 </div>
+                          
             </div>
         </div>
+            
+        
     )
 }
 
