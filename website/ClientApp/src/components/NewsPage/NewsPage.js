cfdd40ed3570674b9fb7afa2600a9f5b716f91f0
Vova <romaniuk.volodymyr.clg@chnu.edu.ua>
2022-02-07 18:26:43 +0200

    gfdgddf

--- website/ClientApp/src/components/NewsPage/NewsPage.js
+++ website/ClientApp/src/components/NewsPage/NewsPage.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+
 import axios from 'axios'
 import '../NewsPage/NewsPage.css'
 function NewsPage() {
@@ -11,40 +12,47 @@ function NewsPage() {
         console.log(result.data);
       });
   }, []);
-
+    
+  
 
 
   return (
-  <div className = "container_all">
-  
+    <div className="newspage">
 
-      <div className="news_header">
-          Новини
-      </div>
-      {news.map((n,index)=>(
-      <div className="news_block">
+        <div className = "container_all">
         
-        <div className="news_title">
-          { n.title}
-        </div>
-        <div className="news_info">
-        <img src={`data:image/gif;base64,${n.image}`}/>
-          <div className="text_and_link">
-            <h2>{n.short_text}</h2>
-           <div className="link_news">
-            <Link key = {n.id} to ={`/news/${n.id}`}>Детальніше</Link>
-            </div>
-           </div>
-        </div>
-        <p>{n.date.substr(0,10)}</p>
-      </div>  
-      ))}
 
+            <div className="news_header">
+                Новини
+            </div>
+          
+            {news.map((n,index)=>(
+            
+            <div className="news_block">
+              
+              <div className="news_title">
+                { n.title}
+              </div>
+              <div className="news_info">
+             <img src={`data:image/gif;base64,${n.image}`}/>
+               <div className="text_and_link">
+                 <h2>{n.short_text}</h2>
+                <div className="link_news">
+                 <Link key = {n.id} to ={`/news/${n.id}`}>Детальніше</Link>
+                  </div>
+                </div>
+             </div>
+             
+             <p>{n.date.substr(0,10)}</p>
+           </div> 
+           
+           ))}
+          </div>
       
       
       
       
-    </div>
+  </div>
   
   );
 }
