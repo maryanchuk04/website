8e400392197b04c3c28eae79cedeb078bb741951
MaksMaryanchuk <lion20914king@gmail.com>
2022-02-08 13:15:53 +0200

    EntrantPageBack

---
+++ website/Controllers/EntrantPageController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using website.Models.PageTemplate;
+using website.Services;
+
+namespace website.Controllers
+{
+    [Route("/entrantpage")]
+    [ApiController]
+    public class EntrantPageController : ControllerBase
+    {
+        private readonly EntrantPageServices _entrantPage;
+
+        public EntrantPageController(EntrantPageServices entrantPage)
+        {
+            _entrantPage = entrantPage;   
+        }
+
+        [HttpGet("/entrantpage")]
+        public ActionResult GetAll() => Ok(_entrantPage.GetAll());
+
+        [HttpGet("/entrantpage/{id}")]
+        public ActionResult GetAll(string id) => Ok(_entrantPage.GetByID(id));
+
+        [HttpPost("/entrantpage/add")]
+        public ActionResult Add([FromBody] EntrantPage entrantPage)
+        {
+            _entrantPage.Insert(entrantPage);
+
+            return Ok(entrantPage);
+        }
+
+        [HttpPost("/entrantpage/upload/image/{id}")]
+        public ActionResult AddImage(string id,[FromForm] IFormFile file)
+        {
+            EntrantPage entrantPage = _entrantPage.GetByID(id);
+
+            if (file.Length > 0)
+            {
+                using (var ms = new MemoryStream())
+                {
+                    file.CopyTo(ms);
+                    var fileBytes = ms.ToArray();
+
+                    entrantPage.image = fileBytes;
+                    _entrantPage.Save(entrantPage);
+                }
+            }
+            return Ok(entrantPage);
+        }
+        [HttpPost("/entrantpage/upload/pdf/{id}")]
+        public ActionResult AddPdf(string id, [FromForm] IFormFile file)
+        {
+            EntrantPage entrantPage = _entrantPage.GetByID(id);
+
+            if (file.Length > 0)
+            {
+                using (var ms = new MemoryStream())
+                {
+                    file.CopyTo(ms);
+                    var fileBytes = ms.ToArray();
+
+                    entrantPage.pdf_link = fileBytes;
+                    _entrantPage.Save(entrantPage);
+                }
+            }
+            return Ok(entrantPage);
+        }
+
+        [HttpPost("/entrantpage/upload/documents/{id}")]
+        public ActionResult AddDocuments(string id, [FromBody] Documents documents)
+        {
+            EntrantPage entrantPage = _entrantPage.GetByID(id);
+            if (entrantPage.document == null)
+            {
+                entrantPage.document = new Documents();
+                entrantPage.document.documents = documents.documents;
+                _entrantPage.Save(entrantPage);
+                return Ok(entrantPage);
+            }
+            else return BadRequest();
+           
+
+        }
+
+
+
+
+    }
+}
---
+++ website/Models/PageTemplate/EntrantPage.cs
@@ -0,0 +1,33 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace website.Models.PageTemplate
+{
+    public class EntrantPage
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+
+        public string id { get; set; } =  MongoDB.Bson.ObjectId.GenerateNewId().ToString();
+        [BsonElement("title")]
+        public string title { get; set; }
+        [BsonElement("pdf_link")]
+        public byte[] pdf_link { get; set; }
+        [BsonElement("vuprobuvannya")]
+        public string vuprobuvannya { get; set; }
+        [BsonElement("document")]
+        public Documents document { get; set; }
+        [BsonElement("image")]
+        public byte[] image { get; set; }
+
+    }
+
+    public class Documents
+    {
+        public List<string> documents { get; set; }
+    } 
+}
