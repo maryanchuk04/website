9011089834507eb396b5cc7c7a92608be7a006b6
Maks <lion20914king@gmail.com>
2022-03-16 13:20:22 +0200

    News

--- website/ClientApp/src/components/NewsPage/NewsPage.js
+++ website/ClientApp/src/components/NewsPage/NewsPage.js
@@ -9,55 +9,27 @@ function NewsPage() {
   
   const [news, setNews] = useState([]);
   const [pagincount,setPagincount]= useState();
-  const [page, setPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(0);
   const [loading, setLoading] = useState(false);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage] = useState(3);
-  const indexOfLastPost = currentPage * postsPerPage;
-  const indexOfFirstPost = indexOfLastPost - postsPerPage;
-  const currentPosts = news.slice(indexOfFirstPost, indexOfLastPost);
- 
+  
   const handleChange = (event, value) => {
     setPagincount(value);
   };
   
-
-
-
   useEffect(() => {
     axios.get("https://bsite.net/IvanovIvan/news").then((res)=>{
       setLoading(true);
       setLoading(false);
       setNews(res.data);
-      setTotalPages(Math.ceil(res.data.length / postsPerPage));
       setPagincount(1);
     })
   }, []);
 
-  const handleClick = num => {
-    setPage(num);
-  }
-  // Change page
-  const paginate = pageNumber => setCurrentPage(pageNumber);
-
-
-  
-
-
-
-  
-    //
-       
-       
   return (
     <div className="newspage">
       <div className="container_all">
       <div className="news_header">
                 Новини
             </div>
-           
-             
              {
                news?.map ((n,index)=>(
                  index>=(pagincount-1)*3 && index<(pagincount)*3 &&
@@ -80,7 +52,7 @@ function NewsPage() {
                ))
              }
              
-               
+
              <Pagination count={Math.ceil(news.length/3)} value={pagincount}  onChange={handleChange} />
           </div>
       </div>
